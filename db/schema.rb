dc8d8b803798e8b17fc6fb611104a729414fb74e
Mehdi FARSI <mehdifarsi.pro@gmail.com>
2016-08-30 22:49:40 +0200

    add history table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160825144257) do
+ActiveRecord::Schema.define(version: 20160830221845) do
 
   create_table "buildings", force: :cascade do |t|
     t.string   "reference"
