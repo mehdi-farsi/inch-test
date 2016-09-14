bd7bd0c64f3e6e304fa276acf142f1a6fd1e112b
Mehdi FARSI <mehdifarsi.pro@gmail.com>
2016-09-14 07:26:39 +0200

    change configs

--- config/initializers/csv_importer.rb
+++ config/initializers/csv_importer.rb
@@ -1,5 +1,12 @@
 require "csv_importer"
 
-CSVImporter.config[:history_whitelisting][:buildings] = [:manager_name]
-CSVImporter.config[:history_whitelisting][:people] = [:email, :home_phone_number, :mobile_phone_number, :address]
-CSVImporter.config[:history_references][:buildings] = :manager_name
+CSVImporter.configure({
+  history_whitelisting: {
+    buildings: [:manager_name],
+    people:    [:email, :phone_number, :cell_phone, :address]
+  },
+  history_references: {
+    buildings: :manager_name,
+    people: :email
+  }
+})
